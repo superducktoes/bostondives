3d6593193ddb69ae21f1d9f0668af385d8e6fe3d
Nick Roy <nick@greynoise.io>
2022-07-23 18:41:59 -0400

    bug fixes and queries

--- mapping.js
+++ mapping.js
@@ -47,8 +47,11 @@ function onLocationError(e) {
                 let popupMessage = json[i]["name"];
 
                 // if mobile add a link to open in google maps
-                if (isMobile) {
+                let ua = navigator.userAgent;
+                if (isMobile && ua.includes("Android")) {
                     popupMessage += `<br><a href='geo: ${lat}, ${long}?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`;
+                } else if (isMobile && ua.includes("iPhone")) {
+                    popupMessage += `<br><a href='https://maps.apple.com/?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`
                 }
 
                 // if the what to order field is popuplated
@@ -84,6 +87,15 @@ var greenIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
+var yellowIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 window.mobileCheck = function () {
     let check = false;
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
@@ -102,6 +114,11 @@ fetch("./locations.json")
     .then((json) => {
 
         let isMobile = window.mobileCheck()
+
+        // check if there is a bar parameter provided
+        let barQuery = window.location.search;
+        const urlParams = new URLSearchParams(barQuery);
+        const plotBarOnMap = urlParams.get('bar');
 
         map.locate({ setView: true, maxZoom: 16 });
         map.on('locationfound', function (e) {
@@ -122,25 +139,34 @@ fetch("./locations.json")
                 distance = calcCrow(userLat, userLong, lat, long)
 
                 // get this info to plot separately
-                if (distance < totalDistance && json[i]["type"] == "bar") {
+                if (distance < totalDistance && json[i]["type"] == "bar" && barQuery == "") {
                     totalDistance = distance;
                     closestBar = "The closest dive bar is: " + json[i]["name"];
                     closestLat = lat;
                     closestLong = long;
                     closestPopupMessage = json[i]["name"];
 
+                    // if the what to order field is popuplated
+                    if (json[i]["whatToOrder"]) {
+                        closestPopupMessage += "<br>What to order: " + json[i]["whatToOrder"];
+                    }
+
                     // if mobile add a link to open in google maps
                     let ua = navigator.userAgent;
 
                     if (isMobile && ua.includes("Android")) {
                         closestPopupMessage += `<br><a href='geo: ${lat}, ${long}?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`;
-                    } else {
+                    } else if (isMobile && ua.includes("iPhone")) {
                         closestPopupMessage += `<br><a href='https://maps.apple.com/?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`
                     }
 
-                    // if the what to order field is popuplated
-                    if (json[i]["whatToOrder"]) {
-                        closestPopupMessage += "<br>What to order: " + json[i]["whatToOrder"];
+                } else if(barQuery) {
+                    closestBar = "Directions to: " + plotBarOnMap;
+                    closestPopupMessage = plotBarOnMap;
+                    if (isMobile && ua.includes("Android")) {
+                        closestPopupMessage += `<br><a href='geo: ${lat}, ${long}?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`;
+                    } else if (isMobile && ua.includes("iPhone")) {
+                        closestPopupMessage += `<br><a href='https://maps.apple.com/?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`
                     }
                 }
 
@@ -153,14 +179,15 @@ fetch("./locations.json")
                 // start creating the popup menu when an icon is clicked on
                 let popupMessage = json[i]["name"];
 
-                // if mobile add a link to open in google maps
-                if (isMobile) {
-                    popupMessage += `<br><a href='geo: ${lat}, ${long}?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`;
-                }
-
                 // if the what to order field is popuplated
                 if (json[i]["whatToOrder"]) {
                     popupMessage += "<br>What to order: " + json[i]["whatToOrder"];
+                }
+
+                if (isMobile && ua.includes("Android")) {
+                    popupMessage += `<br><a href='geo: ${lat}, ${long}?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`;
+                } else if (isMobile && ua.includes("iPhone")) {
+                    popupMessage += `<br><a href='https://maps.apple.com/?q=${lat},${long}' target='_blank' rel='noopener noreferrer'>Directions</a>`
                 }
 
                 // add everything from locations
@@ -169,6 +196,17 @@ fetch("./locations.json")
                     .addTo(map);
             }
 
+
+            if (plotBarOnMap) {
+                for (let i = 0; i < json.length; i++) {
+                    if (json[i]["name"] == plotBarOnMap) {
+                        var lat = parseFloat(json[i]["location"].split(",")[0])
+                        var long = parseFloat(json[i]["location"].split(",")[1])
+                        closestLat = lat;
+                        closestLong = long;
+                    }
+                }
+            }
 
             L.Routing.control({
                 waypoints: [
@@ -182,7 +220,7 @@ fetch("./locations.json")
                 .bindPopup(closestBar).openPopup();
 
             // this gets added a second time to lay over the routing
-            closestMarker = new L.marker([closestLat, closestLong], { icon: redIcon })
+            closestMarker = new L.marker([closestLat, closestLong], { icon: yellowIcon })
                 .bindPopup(closestPopupMessage)
                 .addTo(map);
 
