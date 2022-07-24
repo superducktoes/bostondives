d28376d36ee89385ba778dd90e782fd680304b18
Nick Roy <nick@greynoise.io>
2022-07-24 17:54:41 -0400

    pwa mayber

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open("sw-cache").then(function(cache) {
+            const urls = ["./index/html"]
+            return cache.addAll(urls)
+        })
+    );
+});
+
+self.addEventListener("fetch", functoin(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
