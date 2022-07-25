fa9d62835be6caba5f7fbdb948ec3991012a2e22
Nick Roy <nick@greynoise.io>
2022-07-24 22:29:26 -0400

    i cant type

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
 self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open("sw-cache").then(function(cache) {
-            const urls = ["./index.html"]
-            return cache.add("index.html")
+            return cache.add("index.html");
         })
     );
 });
 
-self.addEventListener("fetch", functoin(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener("fetch", function(e) {
+    e.resoindWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     );
 });
