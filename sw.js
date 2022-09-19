3230cc30ac039d3e8ec439f9624a6363ee6297d1
superducktoes <me@nickroy.org>
2022-09-18 21:56:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,6 @@
 const staticBostonDivesAssets = "bostondives-bar"
 const assets = [
-    "/index.html",
-    "/about.html",
-    "/locations.json",
-    "/manifest.json",
-    "/mapping.js",
-    "/robots.txt",
-    "/stats.html",
-    "/stats.js",
-    "/stylesheet.css",
-    "/sw.js",
-    "/libraries/leaflet-messagebox.css",
-    "/libraries/leaflet-messagebox.js",
-    "/images/beer-svgrepo-com.svg",
-    "/images/closest.png",
-    "/images/directions.png",
-    "/images/drankhere.png",
-    "/images/food.png",
-    "/images/icons8-beeer-64.png",
-    "/images/icons8-twitter-circled-50.png",
-    "/images/insta.svg",
-    "/images/notes.png",
-    "/images/query_bars.png"
+    "/"
 ]
 
 self.addEventListener("install", installEvent => {
