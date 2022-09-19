c5151c40fc622369f6e1627338af895b72bdd79a
superducktoes <me@nickroy.org>
2022-09-18 21:39:01 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ const assets = [
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
         caches.open(staticBostonDivesAssets).then(cache => {
-            cache.addAll(assets)
+            return cache.addAll(assets)
         })
     )
 })
