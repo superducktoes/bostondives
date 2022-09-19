9cf7ee86a67d8dc75e1d44271bda6dab896aa9a6
superducktoes <me@nickroy.org>
2022-09-18 21:06:55 -0400

    updating for pwa

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open("sw-cache").then(function(cache) {
-            const urls = ["index.html", "mapping.js", "about.html", "stylesheet.css", "/libraries", "/images"]
+        caches.open("sw-cache").then(function (cache) {
+            const urls = [
+                "index.html",
+                "about.html",
+                "locations.json",
+                "manifest.json",
+                "mapping.js",
+                "robots.txt",
+                "stats.html",
+                "stats.js",
+                "stylesheet.css",
+                "sw.js",
+                "./libraries/leaflet-messagebox.css",
+                "./libraries/leaflet-messagebox.js",
+                "./images/beer-svgrepo-com.svg",
+                "./images/closest.png",
+                "./images/directions.png",
+                "./images/drankhere.png",
+                "./images/food.png",
+                "./images/icons8-beeer-64.png",
+                "./images/icons8-twitter-circled-50.png",
+                "./images/insta.svg",
+                "./images/notes.png",
+                "./images/query_bars.png"
+            ]
             return cache.addAll(urls)
         })
     );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
     event.resoindWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
         })
     );
