879d64eb74b170f9564e880d320715b66cbb7d98
Nick Roy <nick@greynoise.io>
2022-12-11 21:22:19 -0500

    cleaning up code

--- stats.js
+++ stats.js
@@ -6,25 +6,28 @@ window.onload = function () {
 
             let barsVistedCounter = localStorage.length;
             let barsNotVisitedCounter = json.length - localStorage.length;
-            let barsCompleted = Math.round((barsVistedCounter/json.length) * 100);
+            let barsCompleted = Math.round((barsVistedCounter / json.length) * 100);
 
             let listOfBars = "<ul id='listOfBars' style='list-style-type: none';padding: 0;margin: 0;>"
 
+            // modify the json object based on local storage to update visited bars
             for (let i = 0; i < localStorage.length; i++) {
                 var key = localStorage.key(i);
-                for(let j = 0; j < json.length; j++) {
-                    if(json[j]["name"] == key) {
+                for (let j = 0; j < json.length; j++) {
+                    if (json[j]["name"] == key) {
                         json[j]["visited"] = true;
                     }
                 }
+
             }
 
-            for(let i = 0; i < json.length; i++) {
+            for (let i = 0; i < json.length; i++) {
                 let bullet = "❌"
 
-                if(json[i]["visited"]) {
-                   bullet = "✅" 
+                if (json[i]["visited"]) {
+                    bullet = "✅"
                 }
+
                 listOfBars += `<li>${bullet} <a href="https://bostondives.bar/?bar=${json[i]["name"]}">${json[i]["name"]}</a></li>`
             }
 
