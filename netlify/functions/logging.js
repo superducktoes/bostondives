e5308005857d64b37f3acdc4f00e13998a450a19
superducktoes <me@nickroy.org>
2022-12-28 12:25:45 -0500

    Update logging.js

--- netlify/functions/logging.js
+++ netlify/functions/logging.js
@@ -14,8 +14,7 @@ exports.handler = async (event, context) => {
         applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
     });
 
-    //const basePath = "/api/rest/v2/namespaces/app/collections/logs";
-    const basePath = "/api/rest/v2/KEYSPACES/bostondives/collections/logs";
+    const basePath = "/api/rest/v2/bostondives/app/collections/logs";
     try {
         /*const { data, status } = await astraClient.post(basePath, {
             closestBar: bar,
