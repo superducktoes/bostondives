b3e816df6487d4cdfa03c66219ff83cffb93f1cc
superducktoes <me@nickroy.org>
2022-12-27 21:47:08 -0500

    Update logging.js

--- netlify/functions/logging.js
+++ netlify/functions/logging.js
@@ -3,6 +3,9 @@ exports.handler = async (event, context) => {
     const data = {
         hello: "hello world"
     }
+    console.log("event": event);
+    console.log("context": context);
+    
     return {
         statusCode: 200,
         body: JSON.stringify(data),
