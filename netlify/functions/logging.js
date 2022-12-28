1b06dcd2fb6e09eefb8fa9514565c66c97b51b29
superducktoes <me@nickroy.org>
2022-12-28 14:32:38 -0500

    Update logging.js

--- netlify/functions/logging.js
+++ netlify/functions/logging.js
@@ -3,9 +3,9 @@ exports.handler = async (event, context) => {
     const queryString = event.queryStringParameters;
     const bar = queryString.bar || 'None';
 
-    console.log("bar: ", bar);
-    console.log("event ip: ", event["multiValueHeaders"]["X-Forwarded-For"][0])
-    console.log("query: ", event.multiValueQueryStringParameters)
+    //console.log("bar: ", bar);
+    console.log("event ip: ", event["multiValueHeaders"]["X-Forwarded-For"][0].split(",")[0])
+    console.log("closest dive bar: ", event["multiValueQueryStringParameters"]["bar"])
     const data = {
     }
 
