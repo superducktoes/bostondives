deccece576b701ad29837d7810d316e4478b9e7b
superducktoes <me@nickroy.org>
2022-12-28 11:27:00 -0500

    Update logging.js

--- netlify/functions/logging.js
+++ netlify/functions/logging.js
@@ -1,6 +1,8 @@
+const { createClient } = require("@astrajs/rest");
+
 exports.handler = async (event, context) => {
   
-    const queryString = event.queryStringParameters;
+    /*const queryString = event.queryStringParameters;
     const bar = queryString.bar || 'None';
 
     console.log("bar: ", bar);
@@ -11,5 +13,35 @@ exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify(data),
-    }
+    }*/
+
+    const queryString = event.queryStringParameters;
+    const bar = queryString.bar || 'None';
+    console.log("bar: ", bar);
+
+
+    const astraClient = await createClient({
+        astraDatabaseId: process.env.ASTRA_DB_ID,
+        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+      });
+
+      const basePath = "/api/rest/v2/namespaces/app/collections/logs";
+
+      try {
+        const { data, status } = await astraClient.post(basePath, {
+            closestBar: bar,
+            ipAddress: "0.0.0.0",
+          });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(message);
+        };
+      } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e),
+        }
+      }
 };
