6c400472bdcb948e03a140214e7c951bf43b58f1
superducktoes <me@nickroy.org>
2023-01-11 23:17:45 -0500

    Update mapping.js

--- mapping.js
+++ mapping.js
@@ -240,28 +240,29 @@ function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else { 
-      x.innerHTML = "Geolocation is not supported by this browser.";
+      console.log("Geolocation is not supported by this browser.");
     }
   }
   
   function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    locationText = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
+    console.log(locationText)
   }
   
   function showError(error) {
     switch(error.code) {
       case error.PERMISSION_DENIED:
-        x.innerHTML = "User denied the request for Geolocation."
+       console.log("User denied the request for Geolocation.")
         break;
       case error.POSITION_UNAVAILABLE:
-        x.innerHTML = "Location information is unavailable."
+        console.log("Location information is unavailable.")
         break;
       case error.TIMEOUT:
-        x.innerHTML = "The request to get user location timed out."
+        console.log("The request to get user location timed out.")
         break;
       case error.UNKNOWN_ERROR:
-        x.innerHTML = "An unknown error occurred."
+        console.log("An unknown error occurred.")
         break;
     }
   }
