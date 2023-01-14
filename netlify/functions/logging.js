7fd737ea0ddd07bb479e3d6c52070c08d6a62cc2
superducktoes <me@nickroy.org>
2023-01-14 17:03:07 -0500

    updated logging to use fauna

--- netlify/functions/logging.js
+++ netlify/functions/logging.js
@@ -1,38 +1,3 @@
-/*exports.handler = async (event, context) => {
-  
-    const queryString = event.queryStringParameters;
-    const bar = queryString.bar || 'None';
-    const barSaved = queryString.barSaved || 'None';
-    const error = queryString.error || 'None';
-
-    if(bar != "None"){
-        console.log("closest_bar: ", bar);
-        console.log("useragent: ", event["headers"]["user-agent"]);
-        console.log("clientip: ", event["multiValueHeaders"]["X-Forwarded-For"][0].split(",")[0]);
-    } else if(barSaved != "None"){
-        console.log("saved_bar: ", barSaved);
-        console.log("useragent: ", event["headers"]["user-agent"]);
-        console.log("clientip: ", event["multiValueHeaders"]["X-Forwarded-For"][0].split(",")[0]);
-    } else if(error != "None") {
-        console.log("error_message:", error);
-        console.log("useragent: ", event["headers"]["user-agent"]);
-        console.log("clientip: ", event["multiValueHeaders"]["X-Forwarded-For"][0].split(",")[0]);
-    } else {
-        console.log("Not Sharing Location");
-        console.log("useragent: ", event["headers"]["user-agent"]);
-        console.log("clientip: ", event["multiValueHeaders"]["X-Forwarded-For"][0].split(",")[0]);
-    }
-
-
-    const data = {
-    }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    }
-};*/
-
 const faunadb = require('faunadb');
 
 /* configure faunaDB Client with our secret */
