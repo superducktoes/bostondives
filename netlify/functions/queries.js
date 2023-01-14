48a5a2f124cc5bd62e88a47b831df1a9ddc828df
superducktoes <me@nickroy.org>
2023-01-14 16:00:17 -0500

    Update queries.js

--- netlify/functions/queries.js
+++ netlify/functions/queries.js
@@ -6,42 +6,33 @@ const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET
 });
 
+s
+
 /* export our lambda function as named "handler" export */
 exports.handler = async (event, context) => {
-    const queryString = event.queryStringParameters;
-    const bar = queryString.bar || 'None';
-
     /* parse the string body into a useable JS object */
     const data = JSON.parse(event.body);
     console.log('Function `create` invoked', data);
     const item = {
-        data: bar
+      data: data
     };
     /* construct the fauna query */
     return client
-        .query(q.Create(q.Ref('9d861bad-bf9e-4bdd-92c2-7ceb583e3c15/logs'), item))
-        .then(response => {
-            console.log('success', response);
-            /* Success! return the response with statusCode 200 */
-            return {
-                statusCode: 200,
-                body: JSON.stringify(response)
-            };
-        })
-        .catch(error => {
-            console.log('error', error);
-            /* Error! return the error with statusCode 400 */
-            return {
-                statusCode: 400,
-                body: JSON.stringify(error)
-            };
-        });
-
-    data = {
-    }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    }
-};
+      .query(q.Create(q.Ref('9d861bad-bf9e-4bdd-92c2-7ceb583e3c15/log'), item))
+      .then(response => {
+        console.log('success', response);
+        /* Success! return the response with statusCode 200 */
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response)
+        };
+      })
+      .catch(error => {
+        console.log('error', error);
+        /* Error! return the error with statusCode 400 */
+        return {
+          statusCode: 400,
+          body: JSON.stringify(error)
+        };
+      });
+  };
