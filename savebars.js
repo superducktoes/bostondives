e081745226b2d3ed487b2116ba0412f804245f46
superducktoes <me@nickroy.org>
2023-01-29 18:09:16 -0500

    transfer bars between devices

---
+++ savebars.js
@@ -0,0 +1,20 @@
+// this should always load since we need to populate the top text box with the b64 string
+let bars = JSON.stringify({...localStorage});
+let encoded = btoa(bars);
+document.getElementById('copyb64').value = encoded;
+
+const element = document.getElementById("savebars");
+element.addEventListener('click', function() {
+    try {
+        document.getElementById('loadMessage').innerText = "";
+        let decodedBars = JSON.parse(atob(document.getElementById("pasteb64").value));
+        localStorage.clear();
+        for(let i in decodedBars) {
+            localStorage.setItem(i, true)
+        }
+    } catch(e) {
+        document.getElementById('loadMessage').innerText = e;
+    }
+
+    document.getElementById('loadMessage').innerText = "Bars successfully loaded";
+}, false);
