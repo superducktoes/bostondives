b57bdfd5af46066a888f36c82834c7fb7c54c7f4
superducktoes <me@nickroy.org>
2023-06-10 14:43:12 -0400

    Update ip-rep.js

--- netlify/edge-functions/ip-rep.js
+++ netlify/edge-functions/ip-rep.js
@@ -3,16 +3,31 @@ export default async (request, context) => {
   const BLOCKED_COUNTRY_CODE = "GB";
   const countryCode = "US";
   const countryName = "United States of America";
-  //const { ip } = JSON.parse(request.body);
+  
   console.log(request);
   console.log("---")
   console.log(context);
-  //const PANGEA_ACCESS_TOKEN = process.env.PANGEA_ACCESS_TOKEN;
-  //const PROVIDER = process.env.PROVIDER;
+
   const PANGEA_ACCESS_TOKEN = Netlify.env.get("PANGEA_ACCESS_TOKEN");
   const PROVIDER = Netlify.env.get("PROVIDER");
   const ip = context["ip"];
-  console.log(ip);
+
+  const response = await fetch('https://ip-intel.aws.us.pangea.cloud/v1/reputation', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${PANGEA_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      ip,
+      verbose: false,
+      raw: false,
+      provider: PROVIDER,
+    }),
+  });
+
+  const data = await response.json();
+  const score = data.score;
 
   if (countryCode === BLOCKED_COUNTRY_CODE) {
     return new Response(`We're sorry, you can't access our content from ${countryName}!`, {
