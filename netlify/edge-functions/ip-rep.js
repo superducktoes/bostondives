1607da8a7c888fd5d3a31bbeffe3e6fd113fbeb6
superducktoes <me@nickroy.org>
2023-06-10 14:16:22 -0400

    Update ip-rep.js

--- netlify/edge-functions/ip-rep.js
+++ netlify/edge-functions/ip-rep.js
@@ -1,45 +1,18 @@
-// ipReputation.js
+import { Context } from "https://edge.netlify.com";
 
-const fetch = require('node-fetch');
+export default async (request: Request, context: Context) => {
+  const BLOCKED_COUNTRY_CODE = "GB";
+  const countryCode = context.geo?.country?.code || "US";
+  const countryName = context.geo?.country?.name || "United States of America";
 
-const handler = async (event) => {
-  const { ip } = JSON.parse(event.body);
-
-  const { PANGEA_ACCESS_TOKEN, PROVIDER } = process.env;
-
-  const response = await fetch('https://ip-intel.aws.us.pangea.cloud/v1/reputation', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${PANGEA_ACCESS_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ip,
-      verbose: false,
-      raw: false,
-      provider: PROVIDER,
-    }),
-  });
-
-  const data = await response.json();
-  const score = data.score;
-
-  if (score < 75) {
-    return new Response('', {
-      status: 302,
-      headers: {
-        'Location': 'https://bostondives.com',
-      },
-    });
-  } else {
-    return new Response('Access Denied', {
-      status: 403,
+  if (countryCode === BLOCKED_COUNTRY_CODE) {
+    return new Response(`We're sorry, you can't access our content from ${countryName}!`, {
+      headers: { "content-type": "text/html" },
+      status: 451,
     });
   }
-};
 
-export default handler;
-
-export const config = {
-  path: "/test",
+  return new Response(`Hello there! You can freely access our content from ${countryName}!`, {
+    headers: { "content-type": "text/html" },
+  });
 };
