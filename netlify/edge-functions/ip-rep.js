664640f91ac1572487d7619a19b6da29f71f4e64
superducktoes <me@nickroy.org>
2023-06-10 14:39:04 -0400

    Update ip-rep.js

--- netlify/edge-functions/ip-rep.js
+++ netlify/edge-functions/ip-rep.js
@@ -4,12 +4,14 @@ export default async (request, context) => {
   const countryCode = "US";
   const countryName = "United States of America";
   //const { ip } = JSON.parse(request.body);
-  console.log(request["ip"]);
+  console.log(request);
+  console.log("---")
+  console.log(context);
   //const PANGEA_ACCESS_TOKEN = process.env.PANGEA_ACCESS_TOKEN;
   //const PROVIDER = process.env.PROVIDER;
   const PANGEA_ACCESS_TOKEN = Netlify.env.get("PANGEA_ACCESS_TOKEN");
-  
-  console.log(PANGEA_ACCESS_TOKEN);
+  const PROVIDER = Netlify.env.get("PROVIDER");
+
 
   if (countryCode === BLOCKED_COUNTRY_CODE) {
     return new Response(`We're sorry, you can't access our content from ${countryName}!`, {
