b8f55c3bb22ed48a72eb94ee70ec35ef0ff23791
superducktoes <me@nickroy.org>
2023-06-10 13:59:54 -0400

    Update ip-rep.js

--- netlify/edge-functions/ip-rep.js
+++ netlify/edge-functions/ip-rep.js
@@ -1,32 +1,3 @@
-const axios = require('axios');
+export default () => new Response("Hello world");
 
-exports.handler = async (event, context) => {
-  const ip = event.headers['client-ip'];
-
-  try {
-    const response = await axios.get(`https://api.pangeo.io/ip/${ip}?key=YOUR_PANGEO_API_KEY`);
-    
-    const { reputation } = response.data;
-    
-    if (reputation === 'good') {
-      // IP has a good reputation, allow the request
-      return {
-        statusCode: 200,
-        body: 'Welcome to the website!',
-      };
-    } else {
-      // IP has a bad reputation, reject the request
-      return {
-        statusCode: 403,
-        body: 'Access denied due to IP reputation!',
-      };
-    }
-  } catch (error) {
-    // Handle errors, such as API request failures
-    console.error('Error checking IP reputation:', error);
-    return {
-      statusCode: 500,
-      body: 'Error checking IP reputation',
-    };
-  }
-};
+export const config = { path: "/test" };
