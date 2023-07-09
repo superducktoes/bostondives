64c9f3ecc2c4585afdfad2d61015f33db5028d84
superducktoes <me@nickroy.org>
2023-07-09 14:39:07 -0400

    Update savebars.js

--- savebars.js
+++ savebars.js
@@ -1,13 +1,13 @@
 // this should always load since we need to populate the top text box with the b64 string
 let bars = JSON.stringify({...localStorage});
-let encoded = btoa(bars);
+let encoded = btoa(encodeURIComponent(bars));
 document.getElementById('copyb64').value = encoded;
 
 const element = document.getElementById("savebars");
 element.addEventListener('click', function() {
     try {
         document.getElementById('loadMessage').innerText = "";
-        let decodedBars = JSON.parse(atob(document.getElementById("pasteb64").value));
+        let decodedBars = JSON.parse(decodeURIComponent(atob(document.getElementById("pasteb64")).value));
         localStorage.clear();
         for(let i in decodedBars) {
             localStorage.setItem(i, true)
