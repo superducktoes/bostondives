7eb3c17ea06fd29899fb361fff937c9cc91c2880
superducktoes <me@nickroy.org>
2023-11-13 15:49:15 -0500

    Update logging.js

--- netlify/functions/logging.js
+++ netlify/functions/logging.js
@@ -95,7 +95,7 @@ exports.handler = async (event, context) => {
           const data = JSON.stringify({
             'config_id': 'pci_chp3tsozuiuztyizjpe4kq7i6vuiyytw',
             'event': {
-              'message': postData,
+              'message': JSON.stringify(postData),
             },
           });
 
